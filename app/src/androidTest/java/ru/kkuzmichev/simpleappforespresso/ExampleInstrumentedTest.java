9a658c9c79c75e72d957297b472685459d5c0479
Nikita Klishin <klishin.nd16@gmail.com>
2022-03-21 23:21:24 +0300

    Lab_01

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
@@ -1,14 +1,26 @@
 package ru.kkuzmichev.simpleappforespresso;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.DrawerMatchers.isOpen;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,5 +34,39 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("ru.kkuzmichev.simpleappforespresso", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+
+    @Test
+    public void homeTest() {
+        ViewInteraction settingsButton = onView(
+                withId(R.id.text_home)
+        );
+        settingsButton.check(
+                matches(
+                        withText("This is home fragment")
+                )
+        );
+    }
+
+    @Test
+    public void testProgressBar() {
+        ViewInteraction progressBar = onView(
+                withId(R.id.progress_bar)
+        );
+        ViewInteraction drawer = onView(
+                withId(R.id.drawer_layout)
+        );
+        ViewInteraction navigation = onView(
+                withId(R.id.nav_view)
+        );
+
+        drawer.perform(DrawerActions.open());
+        navigation.perform(NavigationViewActions.navigateTo(R.id.nav_gallery));
+        progressBar.check(doesNotExist());
     }
 }
